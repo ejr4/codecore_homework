fe73f6a0ff1b11c97811764c452271b3cd2ddfce
Eric <ejr4@pm.me>
2018-05-29 14:35:23 -0700

    event listeners on buttons; generated in same loop

--- hang_man/keypad.js
+++ hang_man/keypad.js
@@ -9,11 +9,17 @@ for (let i = a_offset; i < a_offset+ 26; i++){
 
 
 
-let alpha_keys = ["a", "b", "c", "d", "e"];
+//let alpha_keys = ["a", "b", "c", "d", "e"];
+let alpha_keys = alph;
 let html = [];
 for (let i=0; i < alpha_keys.length; i++) {
   html[html.length] =
   `<button id=${alpha_keys[i]} class="button">`+alpha_keys[i]+`</button>`;
+  $("#key-script-section").append(html[i]);
+  $(`#${alpha_keys[i]}`).on("click", event => {
+    const { currentTarget } = event;    
+    $(currentTarget).addClass("button-blue");
+  });
 }
-$("#key-script-section").append(html);
+//$("#key-script-section").append(html);
 
