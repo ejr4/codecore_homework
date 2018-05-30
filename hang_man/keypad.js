cbf1f13e7c6b9b31ff5e793f4cf21ef07159343a
Eric <ejr4@pm.me>
2018-05-29 19:50:58 -0700

    rotating images and monospace implemented.

--- hang_man/keypad.js
+++ hang_man/keypad.js
@@ -1,5 +1,6 @@
 let you_win_html = `<h1>You win!!</h1>`;
-
+let you_lose_html = `<h1>You lose!!</h1>`;
+let wrong_guesses = 0;
 let alph = [];
 let a_offset = 'a'.charCodeAt(0);
 for (let i = a_offset; i < a_offset+ 26; i++){
@@ -20,13 +21,24 @@ for (let i=0; i < alpha_keys.length; i++) {
   $(`#${this_letter}`).on("click", event => {
     const { currentTarget } = event;    
     $(currentTarget).addClass("button-blue");
-    
-    document.querySelectorAll(`.button-${this_letter}`).forEach( node => {
+    let these_nodes = document.querySelectorAll(`.button-${this_letter}`);
+    if ( these_nodes.length == 0 ) {
+      wrong_guesses ++;
+      $("#hanging-man-display").html(
+        `<img src="assets/pic${wrong_guesses}.png"></img>`
+      )
+    }
+    else {
+    these_nodes.forEach( node => {
       node.classList.add('button-red');
       node.classList.remove('button-blank');
     });
+    }
     if (document.querySelectorAll(".button-blank").length == 0){
       $("#alpha-keys-div").append(you_win_html);
+    }
+    if (wrong_guesses >= 6) {
+      $("#alpha-keys-div").append(you_lose_html);
     }
   });
 }
--- hang_man/word_display.js
+++ hang_man/word_display.js
@@ -1,9 +1,1 @@
 
-let letters = word.split('');
-let word_html = [];
-for (let i=0; i < letters.length; i++) {
-  word_html[word_html.length] =
-  `<button id="b-${letters[i]}-i" class="button-display-${letters[i]}">`+letters[i]+`</button>`;
-}
-
-$("#word-display-div").append(word_html);
