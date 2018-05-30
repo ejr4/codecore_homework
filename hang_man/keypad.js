3fac4bea576b36064782a0720ee44ca65dd12e8e
Eric <ejr4@pm.me>
2018-05-29 17:12:02 -0700

    continuing query selection; about to implement if-else letter guessing

--- hang_man/keypad.js
+++ hang_man/keypad.js
@@ -1,4 +1,4 @@
-
+let you_win_html = `<h1>You win!!</h1>`;
 
 let alph = [];
 let a_offset = 'a'.charCodeAt(0);
@@ -13,12 +13,21 @@ for (let i = a_offset; i < a_offset+ 26; i++){
 let alpha_keys = alph;
 let html = [];
 for (let i=0; i < alpha_keys.length; i++) {
+  let this_letter = alpha_keys[i];
   html[html.length] =
-  `<button id=${alpha_keys[i]} class="button">`+alpha_keys[i]+`</button>`;
-  $("#key-script-section").append(html[i]);
-  $(`#${alpha_keys[i]}`).on("click", event => {
+  `<button id=${this_letter} class="button">`+this_letter+`</button>`;
+  $("#alpha-keys-div").append(html[i]);
+  $(`#${this_letter}`).on("click", event => {
     const { currentTarget } = event;    
     $(currentTarget).addClass("button-blue");
+    
+    document.querySelectorAll(`.button-${this_letter}`).forEach( node => {
+      node.classList.add('button-red');
+      node.classList.remove('button-blank');
+    });
+    if (document.querySelectorAll(".button-blank").length == 0){
+      $("#alpha-keys-div").append(you_win_html);
+    }
   });
 }
 //$("#key-script-section").append(html);
