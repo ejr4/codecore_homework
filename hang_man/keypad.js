3df2a490686384ff6cc7db5cf944207ee426b843
Eric <ejr4@pm.me>
2018-05-31 13:41:22 -0700

    refactored functions; two scripts in use effectively sharing variables; custom dental work required

--- hang_man/keypad.js
+++ hang_man/keypad.js
@@ -1,13 +1,11 @@
-let snake_snd = new Audio("assets/snakehiss.wav"); // buffers automatically when created
-
 let wrong_guesses = 0;
-let alph = [];
+let alpha_keys = [];
+let html = [];
+
 let a_offset = 'a'.charCodeAt(0);
 for (let i = a_offset; i < a_offset+ 26; i++){
-  alph.push( String.fromCharCode (i) ) ;
+  alpha_keys.push( String.fromCharCode (i) ) ;
 }
-let alpha_keys = alph;
-let html = [];
 for (let i=0; i < alpha_keys.length; i++) {
   let this_letter = alpha_keys[i];
   html[html.length] =
@@ -17,28 +15,7 @@ for (let i=0; i < alpha_keys.length; i++) {
   $(`#${this_letter}`).on("click", event => {
     const { currentTarget } = event;    
     $(currentTarget).addClass("button-blue");
-    let these_nodes = document.querySelectorAll(`.button-${this_letter}`);
-    if ( these_nodes.length == 0 ) {
-      wrong_guesses ++;
-      $("#hanging-man-display").html(
-        `<img src="assets/pic${wrong_guesses}.png"></img>`
-      );
-      snake_snd.play();
-    }
-    else {
-    these_nodes.forEach( node => {
-      node.classList.add('button-red');
-      node.classList.remove('button-blank');
-    });
-    }
-    if (document.querySelectorAll(".button-blank").length == 0){
-      alert("You live!!");
-      restart_game();
-    }
-    if (wrong_guesses > 6) {
-      alert("You will die for guessing such silly letters!");
-      restart_game();      
-    }
+    check_for_letter(this_letter);
+    end_game_check();
   });
 }
-//$("#key-script-section").append(html);
--- hang_man/main.js
+++ hang_man/main.js
@@ -1,3 +1,8 @@
+let snake_snd = new Audio("assets/snakehiss.wav");
+let win_snd = new Audio("assets/you_win.wav");
+let lose_snd = new Audio("assets/you_lose.wav");
+let correct_snd = new Audio("assets/aspirator.wav");
+
 function restart_game() {
   return new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -6,47 +11,57 @@ function restart_game() {
       }, 2000 );
   });
 }
-
-
-$(() => {
-$(document).on("keydown", e => {
-   const { currentTarget } = e;
-    console.log(e);
-    const { key } = e;
-    console.log(key);
-    let these_nodes = document.querySelectorAll(`.button-${key}`);
-    if ( these_nodes.length == 0 ) {
+function check_for_letter(key) {
+  let matching_nodes = document.querySelectorAll(`.button-${key}`);
+    if ( matching_nodes.length == 0 ) {
         wrong_guesses ++;
-        $("#hanging-man-display").html(
-          `<img src="assets/pic${wrong_guesses}.png"></img>`
-        );
+        pic_change(wrong_guesses);
         snake_snd.play();
       }
       else {
-      these_nodes.forEach( node => {
+      matching_nodes.forEach( node => {
         node.classList.add('button-red');
         node.classList.remove('button-blank');
       });
       }
-      let keys_matching_keydown = document.querySelectorAll(`#${key}`);
-      keys_matching_keydown.forEach( node =>
-        node.classList.add('button-blue'));
+}
+function you_win() {
+  win_snd.play();
+  pic_change("win");
+  alert("Congratulations!!");
+  restart_game();
+}
+function you_lose() {
+  lose_snd.play();
+  alert("HAHAHAHAA!!");
+  restart_game();
+}
+function pic_change(pic_selector) {
+  $("#hanging-man-display").html(
+    `<img src="assets/pic${pic_selector}.png"></img>`
+  );
+}
+function end_game_check() {
+  if (document.querySelectorAll(".button-blank").length == 0){
+    you_win();
+  }
+  if (wrong_guesses > 6) {
+    you_lose();   
+  }
+}
 
-      if (document.querySelectorAll(".button-blank").length == 0){
-        alert("You live!!");
-        restart_game();
-      }
-      if (wrong_guesses > 6) {
-        alert("You will die for guessing such silly letters!");
-        restart_game();
-        //document.location.reload();
-      }
-});
-
-});
-
-////////////
-// const { currentTarget } = event;    
-//     $(currentTarget).addClass("button-blue");
-//     let these_nodes = document.querySelectorAll(`.button-${this_letter}`);
+$(() => {
+$(document).on("keydown", e => {
+  const { currentTarget } = e;
+  const { key } = e;
     
+  check_for_letter(key);
+    
+  let keys_matching_keydown = document.querySelectorAll(`#${key}`);
+  keys_matching_keydown.forEach( node =>
+    node.classList.add('button-blue'));
+
+  end_game_check();
+});
+
+});
