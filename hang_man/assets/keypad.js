cacc073480293dd497c3f204206a930783c70c5f
Eric <ejr4@pm.me>
2018-05-31 15:00:43 -0700

    image and sound assets commmited; images to be modified for final version

--- hang_man/assets/keypad.js
+++
@@ -1,7 +0,0 @@
-let alpha_keys = ["a", "b", "c", "d", "e"];
-let html = [];
-for (let i=0; i < alpha_keys.length; i++) {
-  html[html.length] =
-  `<button id=${alpha_keys[i]} class="button">` + alpha_keys[i] + `</button>`;
-}
-$("#key-script-section").append("HOHOHO");
--- hang_man/main.js
+++ hang_man/main.js
@@ -2,6 +2,18 @@ let snake_snd = new Audio("assets/snakehiss.wav");
 let win_snd = new Audio("assets/you_win.wav");
 let lose_snd = new Audio("assets/you_lose.wav");
 let correct_snd = new Audio("assets/aspirator.wav");
+let wrong_snd_1 = new Audio("assets/wrong1.wav");
+let wrong_snd_2 = new Audio("assets/wrong2.wav");
+let wrong_snd_3 = new Audio("assets/wrong3.wav");
+function sound_fx_pepper() {
+  wrong_snd_array = [wrong_snd_1, wrong_snd_2, wrong_snd_3];
+  let selector = Math.floor(Math.random() * 6);
+  if (selector%2 < 3) {
+    wrong_snd_array[selector].play();
+  }
+}
+
+
 
 function restart_game() {
   return new Promise((resolve, reject) => {
@@ -16,7 +28,7 @@ function check_for_letter(key) {
     if ( matching_nodes.length == 0 ) {
         wrong_guesses ++;
         pic_change(wrong_guesses);
-        snake_snd.play();
+        sound_fx_pepper();
       }
       else {
       matching_nodes.forEach( node => {
